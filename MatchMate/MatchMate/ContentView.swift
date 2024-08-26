9443d789ff241d636513a1f824b37f1112bc0272
Tushar Gupta <tushar.gupta@noice.id>
2024-08-26 22:58:41 +0530

    basic cell UI created.

--- MatchMate/MatchMate/ContentView.swift
+++ MatchMate/MatchMate/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(Color.white)
+                .shadow(radius: 10)
+            
+            VStack(spacing: 25) {
+                
+                VStack(spacing: 20) {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    
+                    VStack(spacing: 10) {
+                        Text("Tushar Gupta")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.teal)
+                        
+                        Text("Plot No GH-02/A, Sector 16, Vaidpura, Greater Noida, Ghaziabad, Uttar Pradesh 201309")
+                            .font(.callout)
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(.gray)
+                    }
+                }
+                
+                HStack(spacing: 40) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 24))
+                            .foregroundColor(.red)
+                            .padding()
+                            .background(Circle().fill(Color.white))
+                            .shadow(radius: 5)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "checkmark")
+                            .font(.system(size: 24))
+                            .foregroundColor(.green)
+                            .padding()
+                            .background(Circle().fill(Color.white))
+                            .shadow(radius: 5)
+                    }
+                }
+            }
         }
-        .padding()
+        .frame(width: .infinity, height: 320)
+        .padding(.horizontal)
     }
 }
 

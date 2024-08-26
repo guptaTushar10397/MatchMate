762b85e205e2b61dec04317a9bda33fa70097ddd
Tushar Gupta <tushar.gupta@noice.id>
2024-08-26 22:58:20 +0530

    Project created.

---
+++ MatchMate/MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Tushar Gupta on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
